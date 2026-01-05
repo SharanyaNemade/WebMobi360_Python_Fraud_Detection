81b78a94e5a507602faf86af74f9f28ad7934e7a
SharanyaNemade <sharanyanemade123@gmail.com>
2026-01-05 11:28:00 +0530

    Initial commit

--- app.py
+++ app.py
@@ -464,6 +464,12 @@ def predict_csv():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        debug=False
+    )
+
 
 
