2bcc6c46c8f6ad63bb5f065f0468a0351d5790f7
SharanyaNemade <sharanyanemade123@gmail.com>
2026-01-05 12:43:38 +0530

    Initial commit

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-gunicorn app:app --bind 0.0.0.0:$PORT --workers 1 --timeout 120
+# gunicorn app:app --bind 0.0.0.0:$PORT --workers 1 --timeout 120
+
+
+gunicorn app:app --bind 0.0.0.0:$PORT
